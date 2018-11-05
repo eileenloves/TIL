b59d9af4b0e27254f7f5307039473b137f8bf066
Daeun Kim <eileenkim1208@gmail.com>
2018-11-05 14:49:57 +0900

    Set the API for giphy

--- VanillaJS/giphy-search-engine/main.js
+++ VanillaJS/giphy-search-engine/main.js
@@ -14,6 +14,11 @@ inputArea.addEventListener('keyup', (e) => {
 });
 
 // 2. API 를 활용해 data를 받는다. 그리고 가공한다.
+const API_KEY = 'sLPioj6JW83rjYtE2oZn9Rbr5SR2VYW0'; // API for giphy app
+let keyword = 'cats';
+const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`; // version 1, refer to DOCS
+console.log(URL);
+
 
 // 3. GIF파일들을 index.html에 밀어 넣는다.
 const pushToDOM = (data) => {
