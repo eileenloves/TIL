a5fc44b70fcd167371dfd0aa6dfb8f6330566bb5
Daeun Kim <eileenkim1208@gmail.com>
2018-11-21 14:19:36 +0900

    finished component

--- ES6/classes.js
+++ ES6/classes.js
@@ -14,8 +14,16 @@ class Audi extends Car{
     super(options);
     this.color = options.color;
   }
+
+  honk(){
+    return '빵빵';
+  }
 }
 
 const car = new Car({title: 'A6'});
 console.log(car);
 console.log(typeof car);
+
+/* 실습1 */
+
+/* 실습2 */
